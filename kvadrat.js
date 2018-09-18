2c3f8a60cb48b30b237e79ec5606d4e3a1401b58
Ambros52 <43155893+Ambros52@users.noreply.github.com>
2018-09-18 09:40:47 +0900

    Update kvadrat.js

--- kvadrat.js
+++ kvadrat.js
@@ -9,7 +9,7 @@ function kvadr() {
 	    var x1 = (-b+Math.sqrt(d))/2/a;
 	    alert("x1 = " + x1);
 	  } else if (d > 0) {
-	  	    var x1 = -b/2/a+Math.sqrt(d);
+	  	    var x1 = (-b+Math.sqrt(d))/2/a;
 	        var x2 = (-b-Math.sqrt(d))/2/a;
 	        alert("D = " + d + "\n" + "x1 = " + x1 + "\n" + "x2 = " + x2);
 	    } else {

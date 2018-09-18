737147389c7f303e525dcc19bb6e1691bb03270e
Ambros52 <43155893+Ambros52@users.noreply.github.com>
2018-09-18 09:40:09 +0900

    Update kvadrat.js

--- kvadrat.js
+++ kvadrat.js
@@ -6,11 +6,11 @@ function kvadr() {
 	if (d < 0) {
 	    alert("No solutions:((");
 	} else if (d == 0) {
-	    var x1 = -b/2/a+Math.sqrt(d);
+	    var x1 = (-b+Math.sqrt(d))/2/a;
 	    alert("x1 = " + x1);
 	  } else if (d > 0) {
 	  	    var x1 = -b/2/a+Math.sqrt(d);
-	        var x2 = -b/2/a-Math.sqrt(d);
+	        var x2 = (-b-Math.sqrt(d))/2/a;
 	        alert("D = " + d + "\n" + "x1 = " + x1 + "\n" + "x2 = " + x2);
 	    } else {
 	    	alert("Enter a number please")
